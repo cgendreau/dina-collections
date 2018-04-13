cede6dba978e1933ada31c68f52ea7cc1470b22c
antonoberg <oberg.anton@gmail.com>
2018-04-13 16:55:10 +0200

    wip

--- packages/ui/src/apps/collectionsUi/viewModules/docs/Component.js
+++ packages/ui/src/apps/collectionsUi/viewModules/docs/Component.js
@@ -23,7 +23,7 @@ class Docs extends Component {
     return (
       <div>
         <ViewWrap leftSidebarEnabled leftSidebarWidth={180}>
-          <PageTemplate hasFixedMenu>
+          <PageTemplate container={false} hasFixedMenu>
             <Switch>
               <Redirect
                 exact
--- packages/ui/src/coreModules/documentation/components/DataModel/index.js
+++ packages/ui/src/coreModules/documentation/components/DataModel/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import { Grid } from 'semantic-ui-react'
+import vis from 'vis'
+import 'vis/dist/vis.css'
 import Model from './Model'
 import Property from './Property'
 
 const specifications = require('common/dist/versions')
+const models = require('common/dist/models.json')
 
 const propTypes = {
   match: PropTypes.shape({
@@ -15,6 +18,68 @@ const propTypes = {
     }).isRequired,
   }).isRequired,
 }
+
+console.log('models', models)
+
+const nodes = []
+const edges = []
+
+const modelsToInclude = [
+  'individual',
+  'identifier',
+  'collectionItem',
+  'featureObservation',
+  'determination',
+  'taxonInformation',
+  'locationInformation',
+  'taxon',
+  'preparationType',
+  'identifierType',
+  'featureType',
+  'identifierType',
+  'observtionInformation',
+  'originInformation',
+  'deathInformation',
+  'collectingInformation',
+  'event',
+  'dataRange',
+  'dataWrapper',
+  'localityInformation',
+  'place',
+]
+
+const modelsToExclude = [
+  'lid',
+  'notDefinedModel',
+  'curatedListType',
+  'curatedList',
+  'externalModelReference',
+  'agent',
+  'curatedEvent',
+  'expedition',
+]
+
+Object.keys(models).forEach(id => {
+  const model = models[id]
+  if (
+    // modelsToInclude.includes(id) &&
+    !modelsToExclude.includes(id) &&
+    model &&
+    model['x-modelType'] === 'model'
+  ) {
+    nodes.push({ id, label: id })
+    if (model.properties) {
+      Object.keys(model.properties).forEach(propertyKey => {
+        const property = model.properties[propertyKey]
+        const ref =
+          (property && property.$ref) || (property.items && property.items.$ref)
+        if (ref) {
+          edges.push({ from: id, to: ref })
+        }
+      })
+    }
+  }
+})
 
 const extractModelFromSpecification = ({ modelId, specification }) => {
   return Object.keys(specification.components.schemas)
@@ -27,6 +92,77 @@ const extractModelFromSpecification = ({ modelId, specification }) => {
 }
 
 class DataModel extends Component {
+  componentDidMount() {
+    const container = document.getElementById('mynetwork')
+    console.log('container', container)
+    // const nodes = new vis.DataSet([
+    //   { id: 1, label: 'individual' },
+    //   { id: 2, label: 'identifier' },
+    //   { id: 3, label: 'collectionItem' },
+    //   { id: 4, label: 'featureObservation' },
+    //   { id: 5, label: 'determination' },
+    //   { id: 6, label: 'taxonInformation' },
+    //   { id: 'mjau', label: 'taxon' },
+    //   // { id: 8, label: 'preparationType' },
+    //   // { id: 9, label: 'identifierType' },
+    //   // { id: 10, label: 'featureType' },
+    //   // { id: 11, label: 'identifierType' },
+    //   // { id: 12, label: 'observtionInformation' },
+    //   // { id: 13, label: 'originInformation' },
+    //   // { id: 14, label: 'deathInformation' },
+    //   // { id: 15, label: 'collectingInformation' },
+    //   // { id: 16, label: 'event' },
+    //   // { id: 17, label: 'dataRange' },
+    //   // { id: 18, label: 'dataWrapper' },
+    //   // { id: 19, label: 'localityInformation' },
+    //   // { id: 20, label: 'place' },
+    // ])
+
+    // // create an array with edges
+    // const edges = new vis.DataSet([
+    //   { from: 'mjau', to: 5, label: 'has many' },
+    //   { from: 'mjau', to: 6 },
+    //   { from: 1, to: 4 },
+    //   { from: 1, to: 5 },
+    //   { from: 1, to: 6 },
+    //   { from: 1, to: 3 },
+    //   { from: 1, to: 2 },
+    // ])
+    const data = {
+      nodes: nodes,
+      edges: edges,
+    }
+    const options = {
+      interaction: {
+        dragNodes: false,
+        dragView: false,
+        zoomView: false,
+      },
+      layout: {
+        randomSeed: 3,
+      },
+      edges: {
+        arrows: {
+          to: {
+            enabled: true,
+            type: 'arrow',
+          },
+        },
+      },
+      nodes: {
+        font: {
+          size: 20,
+        },
+        shape: 'box',
+      },
+    }
+
+    // initialize your network!
+    const network = new vis.Network(container, data, options)
+    network.on('click', data => {
+      console.log('data', data)
+    })
+  }
   render() {
     const {
       match: { params: { modelId, parameterId, schemaVersion } },
@@ -58,7 +194,20 @@ class DataModel extends Component {
       }
     }
 
-    return <Model model={model} version={schemaVersion} />
+    return (
+      <div>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Model model={model} version={schemaVersion} />
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <div style={{ width: '100%', height: 700 }} id="mynetwork" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    )
   }
 }
 
