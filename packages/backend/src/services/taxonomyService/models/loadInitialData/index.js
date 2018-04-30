120fecc203cd729bc84018eb1727d881fc706a38
Fredrik Olovsson <fogithub@gmail.com>
2018-04-30 18:45:01 +0200

    fixup backend initial data

--- packages/backend/src/services/taxonomyService/models/loadInitialData/index.js
+++ packages/backend/src/services/taxonomyService/models/loadInitialData/index.js
@@ -5,14 +5,19 @@ module.exports = function loadInitialData({ models }) {
   const taxa = readInitialData('taxa')
   const taxonNames = readInitialData('taxonNames')
 
-  const taxonItems = !taxa
-    ? null
-    : taxa.map(taxon => {
+  const taxaByIdMap = !taxa
+    ? {}
+    : taxa.reduce((map, taxon) => {
         const { id, parentId, ...rest } = taxon
 
         const resource = {
           doc: deleteNullProperties(rest),
           id,
+          relationships: {
+            taxonNames: [],
+          },
+          synonyms: [],
+          vernacularNames: [],
           versionId: id,
         }
 
@@ -20,8 +25,10 @@ module.exports = function loadInitialData({ models }) {
           resource.parentVersionId = parentId
         }
 
-        return resource
-      })
+        map[id] = resource // eslint-disable-line no-param-reassign
+
+        return map
+      }, {})
 
   const taxonNameItems = !taxonNames
     ? null
@@ -46,18 +53,46 @@ module.exports = function loadInitialData({ models }) {
           versionId: id,
         }
 
+        const relatedTaxonName = {
+          id,
+          type: 'taxonName',
+        }
+
         if (acceptedToTaxonId) {
           resource.acceptedToTaxonVersionId = acceptedToTaxonId
+
+          taxaByIdMap[acceptedToTaxonId].acceptedTaxonName = relatedTaxonName
+          taxaByIdMap[acceptedToTaxonId].relationships.taxonNames.push(
+            relatedTaxonName
+          )
         }
+
         if (synonymToTaxonId) {
           resource.synonymToTaxonVersionId = synonymToTaxonId
+
+          taxaByIdMap[synonymToTaxonId].synonyms.push(relatedTaxonName)
+          taxaByIdMap[synonymToTaxonId].relationships.taxonNames.push(
+            relatedTaxonName
+          )
         }
+
         if (vernacularToTaxonId) {
           resource.vernacularToTaxonVersionId = vernacularToTaxonId
+
+          taxaByIdMap[vernacularToTaxonId].vernacularNames.push(
+            relatedTaxonName
+          )
+          taxaByIdMap[vernacularToTaxonId].relationships.taxonNames.push(
+            relatedTaxonName
+          )
         }
 
         return resource
       })
+
+  const taxonItems = Object.values(taxaByIdMap).sort((a, b) => {
+    return a.id < b.id ? -1 : 1
+  })
 
   const taxonItemsPromise = taxonItems
     ? models.taxon.bulkCreate(taxonItems)
